2dcbbb04946e125a4ea38d0a77bbd5d0a4f93bca
subhasishchow001 <subhasishchowdhury001@gmail.com>
2023-02-11 16:00:21 +0530

    secoend commit

--- downloader.py
+++ downloader.py
@@ -1,19 +1,41 @@
+# import Module
+from selenium import webdriver
 import time
-from selenium.webdriver import Firefox
-from selenium.webdriver.firefox.options import Options
 
-with open("/home/associates/Uji.txt", "r") as file:
-    for line in file:
-        print(line)
-        time.sleep(1)
-    #     opts=Options()
-    #     opts.set_headless()
-    #     assert opts.headless
-    #     browser = Chrome(options=opts)
-    # browser.get('https://duckduckgo.com')
-    # search_form = browser.find_element_by_id('search_form_input_homepage')
-    # search_form.send_keys(line)
-    # search_form.submit()
-        
-        ####change this#####   
+# Create Chrome Object
+
+# driver = webdriver.Chrome('/home/associates/.wdm/drivers/chromedriver/linux64/109.0.5414.74/chromedriver')
+with open("/home/associates/url.txt", "r") as file:
+    lines=file.readlines()
+    count=0
+    for line in lines:
+        count +=1
+        print (line)
+
+
+#   for line in file:
+#       print(line)
+# time.sleep(1)
+
+# def webline(user_name):
+    
+    # Open the url
+    # driver.get('https://www.savethevideo.com/vimeo-downloader')
+
+    # Username for
+    # username = driver.find_element_by_xpath('//*[@id="url"]')
+    # username.send_keys(user_name)
+
+    # Password for login
+    # password = driver.find_element_by_xpath('//*[@id="user_pass"]')
+    # password.send_keys(pass_word)
+
+    # Click on signin button
+    # signin = driver.find_element_by_xpath(
+    #     '//*[@id="wp-submit"]')
+    # signin.click()
+
+
+# webline(line)
+
     
