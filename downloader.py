a747b703dad601d60c4cb44f932a4d4e4bed3c86
subhasishchow001 <subhasishchowdhury001@gmail.com>
2023-02-14 10:54:43 +0530

    newcommit

--- downloader.py
+++ downloader.py
@@ -3,7 +3,6 @@ from selenium import webdriver
 import time
 
 
-
 #opening file
 with open("/home/associates/url.txt", "r") as file:
 	for line in file:
@@ -21,6 +20,15 @@ with open("/home/associates/url.txt", "r") as file:
             start = driver.find_element_by_class_name(
                 '//button[@class="flex items-center justify-center rounded-md shadow-sm border focus:outline-none relative bg-gray-800 hover:bg-gray-900 border-transparent focus:ring-gray-500 focus:ring-2 focus:ring-offset-2 px-6 py-3 text-base text-white mt-3 sm:mt-0 w-full sm:w-auto sm:ml-3"]')
             start.click()
+
+            #close the newly opened window
+            driver.get(url)
+            driver.close()
+
+            #pressing the download button
+            download = driver.find_element_by_class_name(
+                '//button[@class="flex items-center justify-center rounded-md shadow-sm border focus:outline-none relative bg-blue-500 hover:bg-blue-600 border-transparent focus:ring-blue-500 focus:ring-2 focus:ring-offset-2 px-4 py-2 text-sm text-white w-full mt-4"]')
+            download.click()
             
 
          webline(line)
